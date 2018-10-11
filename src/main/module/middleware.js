4381afb10fd5770ce7de9ec7982f8128547e672a
simonpai <simon.pai@gmail.com>
2018-10-11 21:36:12 +0800

    Middleware module skeleton

---
+++ src/main/module/middleware.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+export default class MiddlewareManager {
+
+  constructor(rules) {
+    this._rules = rules;
+    rules.events.on('change', () => {
+      delete this._middlewareCache;
+    });
+
+    // go through the getter everytime
+    Object.defineProperty(this, 'middleware', {
+      value: (req, res, next) => this._middleware(req, res, next)
+    })
+  }
+
+  get _middleware() {
+    return this._middlewareCache || (this._middlewareCache = this._computeMiddleware());
+  }
+
+  _computeMiddleware() {
+    return (this._rules.output.middlewares || [])
+      .sequence()
+      .fold(express(), (app, m) => {
+        app.use(m.path, m.handler);
+        return app;
+      });
+  }
+
+  close() {}
+
+}
