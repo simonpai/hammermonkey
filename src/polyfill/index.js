1b8280d726fe6c79d3ff59cadfa94b590a0068b3
simonpai <simon.pai@gmail.com>
2018-10-28 22:35:05 +0800

    Add @inspect decorator

--- src/polyfill/index.js
+++ src/polyfill/index.js
@@ -3,3 +3,43 @@ import { asSequence } from 'sequency';
 Array.prototype.sequence = function() {
   return asSequence(this);
 };
+
+let _global = undefined;
+try {
+  _global = window;
+} catch(e) {
+  _global = global;
+}
+
+function _inspect(f, name, mode) {
+  return function() {
+    /* eslint-disable no-console */
+    (mode & 1) && console.log('[' + name + '] arguments:', ...arguments);
+    try {
+      const r = f.apply(this, arguments);
+      (mode & 2) && console.log('[' + name + '] return:', r);
+      return r;
+    } catch(err) {
+      console.log('[' + name + '] error:', err);
+      throw err;
+    }
+    /* eslint-enable no-console */
+  }
+}
+
+function inspect(target, name, descriptor) {
+  const prefix = target.constructor.name + '.';
+  if (typeof descriptor.value === 'function') {
+    descriptor.value = _inspect(descriptor.value, prefix + name, 3);
+  }
+  if (descriptor.get) {
+    descriptor.get = _inspect(descriptor.get, prefix + 'get ' + name, 2);
+  }
+  if (descriptor.set) {
+    descriptor.set = _inspect(descriptor.set, prefix + 'set ' + name, 1);
+  }
+}
+
+Object.defineProperty(_global, 'inspect', {
+  value: inspect
+});
